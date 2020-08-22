54fa20a1b69fa025e115afee92f1bd0a34b4dd99
Yizhar (Izzy) Toren <1861565+ytoren@users.noreply.github.com>
2020-08-22 22:37:15 +0200

    fix documentation & license

--- R/matrix_shuffle.R
+++ R/matrix_shuffle.R
@@ -8,7 +8,7 @@ row_shuffle_matrix_ <- function(rows) {
   
 #' @title Re-order a similarity matrix
 #' @description Re-order columns / rows of a similarity matrix based on a given permutation.
-#' @param m Matrix
+#' @param X Matrix
 #' @param row_order Integer vector, a permutation of \code{1:nrow(m)}.
 #' @return A re-arranged similarity matrix (sparse & symmetric)
 sim_matrix_shuffle <- function(X, row_order) {
