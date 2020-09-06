91eed48e9f930e16e058df3e60f0058f02de679a
Yizhar (Izzy) Toren <1861565+ytoren@users.noreply.github.com>
2020-09-06 10:18:21 +0200

    Row order does not matter

--- R/merge_by_partition.R
+++ R/merge_by_partition.R
@@ -1,78 +1,53 @@
-#' @keywords internal
-merge_rows_ <- function(m, row_range, agg = Matrix::colSums) {
-  N <- nrow(m)
-  end <- max(row_range)
-  start <- min(row_range)
-  
-  if (end > N | start < 1) { stop('start / end should match matrix size') }
-  if (start == 1 & end == N) { return(agg(m)) }
-  
-  if (start == 1) {
-    m_above <- NULL
-  } else {
-    m_above <- m[1:(start - 1), ]
-  }
-  
-  if (end == N) {
-    m_below <- NULL
-  } else {
-    m_below <- m[(end + 1):N, ]
-  }
-  
-  return(rbind(m_above, agg(m[start:end, ]), m_below))
-}
-
+#' @importFrom parallel mclapply
+#' @importFrom Matrix Matrix
+#' @importFrom Matrix t
 
 #' @keywords internal
-merge_rows_by_partition_ <- function(m, partition, merge_order, agg = Matrix::colSums) {
-  for (p in merge_order) {
-    if (length(partition[[p]]) == 1) {next}
-    m <- merge_rows_(m, partition[[p]], agg)
+is_partition_ <- function(partition, first, last) {
+  p <- unlist(partition)
+  if (length(p) == last - first + 1) {
+    if (any(sort(p) != first:last)) {
+      return(TRUE)
+    }
   }
-  
-  rownames(m) <- c()
-  return(m)
+  return(FALSE)
 }
 
+#' @keywords internal
+merge_rows_by_partition_ <- function(m, partition, agg, n_cpu) {
+  f <- function(p) {Matrix::Matrix(agg(m[p, , drop = FALSE]), sparse = TRUE, nrow = 1)}
 
-#' @title Merge matrix rows by a partition  
-#' @description Merge matrix rows according to a continuous partition of row indices 
+  if (n_cpu==1) {
+    s <- lapply(partition, f)
+  } else {
+    s <- parallel::mclapply(partition, f, mc.cores = n_cpu)
+  }
+
+  return(do.call(rbind, s))
+}
+
+#' @title Merge matrix rows/columns by a partition  
+#' @description Merge matrix rows/columns according to a partition of row indices 
 #' @param m A matrix
-#' @param partition A list of integer vectors. Each vector can contain all rows in the group or just first & last rows (min/max will be used to extract range)
+#' @param partition A list of integer vectors, which makes a full partition of the rows of \code{m}
 #' @param agg Function. The function that will be called to merge the rows. Must return a matrix with the same number of columns and as many rows as needed. Default is \code{colSums}.
+#' @param n_cpu Integer. Number of cores to be used for the calculation, passed to the parameter \code{mc.cores} of the function \code{mclapply}. Default is 1 which means the "regular" \code{lapply} will be used.
+#' @param check Logical. Should the function check if the provided partition is valid? Default is \code{FALSE}.
 #' @return A new matrix with less (or more?) rows, depending on the aggregation function.
-merge_by_partition <- function(m, partition, agg = Matrix::colSums) {
-  ## Check that partition is... a partition
-  if (!all(sort(unlist(partition)) == 1:nrow(m))) {stop('Missing / too many indices in partition')}
-  invisible(
-    lapply(
-      partition, 
-      function(x) {
-        if (!all(sort(x) == min(x):max(x))) {
-          stop(paste0('Non-continuous partition: ', paste(x, collapse = ',')))
-        }
-      }
-    )
-  )
-  
-  # To maintain index relevance we start from the last partition and go back
-  merge_order <- order(
-    unlist(lapply(partition, min)), 
-    decreasing = TRUE
-  )
+#' @export
+merge_by_partition <- function(m, partition, agg = Matrix::colSums, n_cpu = 1, check = FALSE) {
+  if (check) {
+    if (!is_partition_(partition, 1, nrow(m))) {
+      stop('Partition check failed. Please specify a valid partition')
+    }
+  }
 
   # Merge rows, transpose (now cols are merged) and merge rows again
   return(
     merge_rows_by_partition_(
-      t(
-        merge_rows_by_partition_(
-          m, 
-          partition, 
-          merge_order
-        )
-      ), 
-      partition, 
-      merge_order)
+      Matrix::t(merge_rows_by_partition_(m, partition, agg, n_cpu)),
+      partition, agg, n_cpu
+    )
   )
 }
 
--- tests/testthat/test-merge_by_partition.R
+++ tests/testthat/test-merge_by_partition.R
@@ -1,27 +1,21 @@
 context('Col/Row Merges')
 
 test_that('Errors work', {
-  expect_error(merge_rows_(m, -1:1), regexp = 'match')
-})
-
-test_that('Full returns', {
-  expect_equal(merge_rows_(m, 1:nrow(m)), Matrix::colSums(m))
-})
-
-test_that('row merge for a single block', {
-  expect_false(any(merge_rows_(m, partition[[1]]) != m_merge_first))
-})
-
-test_that('partition row merges', {
-  expect_false(any(merge_rows_by_partition_(m, partition, merge_order) != m_merge_rows))
+  expect_error(merge_by_partition(m = m, partition = list(-1:1), check = TRUE), regexp = 'valid partition')
 })
 
 test_that('Bad partition', {
-  expect_error(merge_by_partition(m, partition = list(c(2,4), c(5,6), 1)), regexp = 'Missing')
+  expect_error(merge_by_partition(m, partition = list(c(2,4), c(5,6), 1), check = TRUE), regexp = 'valid partition')
+})
+
+test_that('Full returns', {
+  expect_equal(merge_rows_by_partition_(m, list(1:nrow(m)), Matrix::colSums, 1)[1, ], Matrix::colSums(m))
+})
+
+test_that('partition row merges', {
+  expect_false(any(merge_rows_by_partition_(m, partition, Matrix::colSums, 1) != m_merge_rows))
 })
 
 test_that('partition cols & rows', {
   expect_false(any(merge_by_partition(m, partition) != m_merged))
 })
-
-
